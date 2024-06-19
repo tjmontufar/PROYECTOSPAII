48c0193b6de9a0f839409fd0cac33a176947a2e3
tjmontufar <tjmontufar@uth.hn>
2024-06-18 21:55:45 -0600

    Proyecto Primer Parcial

---
+++ DEPARTAMENTOS/9_Proyecto.py
@@ -0,0 +1,559 @@
+departamentos_municipios = {
+    '01': {
+        'nombre': 'Atlántida',
+        'municipios': {
+            '01': 'La Ceiba',
+            '02': 'El Porvenir',
+            '03': 'Esparta',
+            '04': 'Jutiapa',
+            '05': 'La Masica',
+            '06': 'San Francisco',
+            '07': 'Tela',
+            '08': 'Arizona'
+        }
+    },
+    '02': {
+        'nombre': 'Colón',
+        'municipios': {
+            '01': 'Trujillo',
+            '02': 'Balfate',
+            '03': 'Iriona',
+            '04': 'Limón',
+            '05': 'Sabá',
+            '06': 'Santa Fe',
+            '07': 'Santa Rosa de Aguán',
+            '08': 'Sonaguera',
+            '09': 'Tocoa',
+            '10': 'Bonito Oriental'
+        }
+    },
+    '03': {
+        'nombre': 'Comayagua',
+        'municipios': {
+            '01': 'Comayagua',
+            '02': 'Ajuterique',
+            '03': 'El Rosario',
+            '04': 'Esquías',
+            '05': 'Humuya',
+            '06': 'La Libertad',
+            '07': 'Lamaní',
+            '08': 'La Trinidad',
+            '09': 'Lejamaní',
+            '10': 'Meámbar',
+            '11': 'Minas de Oro',
+            '12': 'Ojos de Agua',
+            '13': 'San Jerónimo',
+            '14': 'San José de Comayagua',
+            '15': 'San José del Potrero',
+            '16': 'San Luis',
+            '17': 'San Sebastián',
+            '18': 'Siguatepeque',
+            '19': 'Villa de San Antonio',
+            '20': 'Las Lajas',
+            '21': 'Taulabé'
+        }
+    },
+    '04': {
+        'nombre': 'Copán',
+        'municipios': {
+            '01': 'Santa Rosa de Copán',
+            '02': 'Cabañas',
+            '03': 'Concepción',
+            '04': 'Copán Ruinas',
+            '05': 'Corquín',
+            '06': 'Cucuyagua',
+            '07': 'Dolores',
+            '08': 'Dulce Nombre',
+            '09': 'El Paraíso',
+            '10': 'Florida',
+            '11': 'La Jigua',
+            '12': 'La Unión',
+            '13': 'Nueva Arcadia',
+            '14': 'San Agustín',
+            '15': 'San Antonio',
+            '16': 'San Jerónimo',
+            '17': 'San José',
+            '18': 'San Juan de Opoa',
+            '19': 'San Nicolás',
+            '20': 'San Pedro',
+            '21': 'Santa Rita',
+            '22': 'Trinidad de Copán',
+            '23': 'Veracruz'
+        }
+    },
+    '05': {
+        'nombre': 'Cortés',
+        'municipios': {
+            '01': 'San Pedro Sula',
+            '02': 'Choloma',
+            '03': 'Omoa',
+            '04': 'Pimienta',
+            '05': 'Potrerillos',
+            '06': 'Puerto Cortés',
+            '07': 'San Antonio de Cortés',
+            '08': 'San Francisco de Yojoa',
+            '09': 'San Manuel',
+            '10': 'Santa Cruz de Yojoa',
+            '11': 'Villanueva',
+            '12': 'La Lima'
+        }
+    },
+    '06': {
+        'nombre': 'Choluteca',
+        'municipios': {
+            '01': 'Choluteca',
+            '02': 'Apacilagua',
+            '03': 'Concepción de María',
+            '04': 'Duyure',
+            '05': 'El Corpus',
+            '06': 'El Triunfo',
+            '07': 'Marcovia',
+            '08': 'Morolica',
+            '09': 'Namasigüe',
+            '10': 'Orocuina',
+            '11': 'Pespire',
+            '12': 'San Antonio de Flores',
+            '13': 'San Isidro',
+            '14': 'San José',
+            '15': 'San Marcos de Colón',
+            '16': 'Santa Ana de Yusguare'
+        }
+    },
+    '07': {
+        'nombre': 'El Paraíso',
+        'municipios': {
+            '01': 'Yuscarán',
+            '02': 'Alauca',
+            '03': 'Danlí',
+            '04': 'El Paraíso',
+            '05': 'Güinope',
+            '06': 'Jacaleapa',
+            '07': 'Liure',
+            '08': 'Morocelí',
+            '09': 'Oropolí',
+            '10': 'Potrerillos',
+            '11': 'San Antonio de Flores',
+            '12': 'San Lucas',
+            '13': 'San Matías',
+            '14': 'Soledad',
+            '15': 'Teupasenti',
+            '16': 'Texiguat',
+            '17': 'Vado Ancho',
+            '18': 'Yauyupe',
+            '19': 'Trojes'
+        }
+    },
+    '08': {
+        'nombre': 'Francisco Morazán',
+        'municipios': {
+            '01': 'Distrito Central',
+            '02': 'Alubarén',
+            '03': 'Cedros',
+            '04': 'Curarén',
+            '05': 'El Porvenir',
+            '06': 'Guaimaca',
+            '07': 'La Libertad',
+            '08': 'La Venta',
+            '09': 'Lepaterique',
+            '10': 'Maraita',
+            '11': 'Marale',
+            '12': 'Nueva Armenia',
+            '13': 'Ojojona',
+            '14': 'Orica',
+            '15': 'Reitoca',
+            '16': 'Sabanagrande',
+            '17': 'San Antonio de Oriente',
+            '18': 'San Buenaventura',
+            '19': 'San Ignacio',
+            '20': 'San Juan de Flores',
+            '21': 'San Miguelito',
+            '22': 'Santa Ana',
+            '23': 'Santa Lucía',
+            '24': 'Talanga',
+            '25': 'Tatumbla',
+            '26': 'Valle de Ángeles',
+            '27': 'Villa de San Francisco',
+            '28': 'Vallecillo'
+        }
+    },
+    '09': {
+        'nombre': 'Gracias a Dios',
+        'municipios': {
+            '01': 'Puerto Lempira',
+            '02': 'Brus Laguna',
+            '03': 'Ahuas',
+            '04': 'Juan Francisco Bulnes',
+            '05': 'Ramón Villeda Morales',
+            '06': 'Wampusirpi'
+        }
+    },
+    '10': {
+        'nombre': 'Intibucá',
+        'municipios': {
+            '01': 'La Esperanza',
+            '02': 'Camasca',
+            '03': 'Colomoncagua',
+            '04': 'Concepción',
+            '05': 'Dolores',
+            '06': 'Intibucá',
+            '07': 'Jesús de Otoro',
+            '08': 'Magdalena',
+            '09': 'Masaguara',
+            '10': 'San Antonio',
+            '11': 'San Isidro',
+            '12': 'San Juan',
+            '13': 'San Marcos de la Sierra',
+            '14': 'San Miguelito',
+            '15': 'Santa Lucía',
+            '16': 'Yamaranguila',
+            '17': 'San Francisco de Opalaca'
+        }
+    },
+    '11': {
+        'nombre': 'Islas de la Bahía',
+        'municipios': {
+            '01': 'Roatán',
+            '02': 'Guanaja',
+            '03': 'José Santos Guardiola',
+            '04': 'Utila'
+        }
+    },
+    '12': {
+        'nombre': 'La Paz',
+        'municipios': {
+            '01': 'La Paz',
+            '02': 'Aguanqueterique',
+            '03': 'Cabañas',
+            '04': 'Cane',
+            '05': 'Chinacla',
+            '06': 'Guajiquiro',
+            '07': 'Lauterique',
+            '08': 'Marcala',
+            '09': 'Mercedes de Oriente',
+            '10': 'Opatoro',
+            '11': 'San Antonio del Norte',
+            '12': 'San José',
+            '13': 'San Juan',
+            '14': 'San Pedro de Tutule',
+            '15': 'Santa Ana',
+            '16': 'Santa Elena',
+            '17': 'Santa María',
+            '18': 'Santiago de Puringla',
+            '19': 'Yarula'
+        }
+    },
+    '13': {
+        'nombre': 'Lempira',
+        'municipios': {
+            '01': 'Gracias',
+            '02': 'Belén',
+            '03': 'Candelaria',
+            '04': 'Cololaca',
+            '05': 'Erandique',
+            '06': 'Gualcince',
+            '07': 'Guarita',
+            '08': 'La Campa',
+            '09': 'La Iguala',
+            '10': 'Las Flores',
+            '11': 'La Unión',
+            '12': 'La Virtud',
+            '13': 'Lepaera',
+            '14': 'Mapulaca',
+            '15': 'Piraera',
+            '16': 'San Andrés',
+            '17': 'San Francisco',
+            '18': 'San Juan Guarita',
+            '19': 'San Manuel Colohete',
+            '20': 'San Rafael',
+            '21': 'San Sebastián',
+            '22': 'Santa Cruz',
+            '23': 'Talgua',
+            '24': 'Tambla',
+            '25': 'Tomalá',
+            '26': 'Valladolid',
+            '27': 'Virginia',
+            '28': 'San Marcos de Caiquín'
+        }
+    },
+    '14': {
+        'nombre': 'Ocotepeque',
+        'municipios': {
+            '01': 'Ocotepeque',
+            '02': 'Belén Gualcho',
+            '03': 'Concepción',
+            '04': 'Dolores Merendón',
+            '05': 'Fraternidad',
+            '06': 'La Encarnación',
+            '07': 'La Labor',
+            '08': 'Lucerna',
+            '09': 'Mercedes',
+            '10': 'San Fernando',
+            '11': 'San Francisco del Valle',
+            '12': 'San Jorge',
+            '13': 'San Marcos',
+            '14': 'Santa Fé',
+            '15': 'Sensenti',
+            '16': 'Sinuapa'
+        }
+    },
+    '15': {
+        'nombre': 'Olancho',
+        'municipios': {
+            '01': 'Juticalpa',
+            '02': 'Campamento',
+            '03': 'Catacamas',
+            '04': 'Concordia',
+            '05': 'Dulce Nombre de Culmí',
+            '06': 'El Rosario',
+            '07': 'Esquipulas del Norte',
+            '08': 'Gualaco',
+            '09': 'Guarizama',
+            '10': 'Guata',
+            '11': 'Guayape',
+            '12': 'Jano',
+            '13': 'La Unión',
+            '14': 'Mangulile',
+            '15': 'Manto',
+            '16': 'Salamá',
+            '17': 'San Esteban',
+            '18': 'San Francisco de Becerra',
+            '19': 'San Francisco de la Paz',
+            '20': 'Santa María del Real',
+            '21': 'Silca',
+            '22': 'Yocón',
+            '23': 'Patuca'
+        }
+    },
+    '16': {
+        'nombre': 'Santa Bárbara',
+        'municipios': {
+            '01': 'Santa Bárbara',
+            '02': 'Arada',
+            '03': 'Atima',
+            '04': 'Azacualpa',
+            '05': 'Ceguaca',
+            '06': 'Colinas',
+            '07': 'Concepción del Norte',
+            '08': 'Concepción del Sur',
+            '09': 'Chinda',
+            '10': 'El Níspero',
+            '11': 'Gualala',
+            '12': 'Ilama',
+            '13': 'Macuelizo',
+            '14': 'Naranjito',
+            '15': 'Nueva Celilac',
+            '16': 'Petoa',
+            '17': 'Protección',
+            '18': 'Quimistán',
+            '19': 'San Francisco de Ojuera',
+            '20': 'San Luis',
+            '21': 'San Marcos',
+            '22': 'San Nicolás',
+            '23': 'San Pedro Zacapa',
+            '24': 'Santa Rita',
+            '25': 'San Vicente Centenario',
+            '26': 'Trinidad',
+            '27': 'Las Vegas',
+            '28': 'Nueva Frontera'
+        }
+    },
+    '17': {
+        'nombre': 'Valle',
+        'municipios': {
+            '01': 'Nacaome',
+            '02': 'Alianza',
+            '03': 'Amapala',
+            '04': 'Aramecina',
+            '05': 'Caridad',
+            '06': 'Goascorán',
+            '07': 'Langue',
+            '08': 'San Francisco de Coray',
+            '09': 'San Lorenzo'
+        }
+    },
+    '18': {
+        'nombre': 'Yoro',
+        'municipios': {
+            '01': 'Yoro',
+            '02': 'Arenal',
+            '03': 'El Negrito',
+            '04': 'El Progreso',
+            '05': 'Jocón',
+            '06': 'Morazán',
+            '07': 'Olanchito',
+            '08': 'Santa Rita',
+            '09': 'Sulaco',
+            '10': 'Victoria',
+            '11': 'Yorito'
+        }
+    }
+}
+'''
+    ALUMNOS:
+    - ILIANA LICETH ZUNIGA ENAMORADO
+    - DIANY LIZBETH ENAMORADO FERNANDEZ
+    - ANDERSON JAIR GARCIA MENJIVAR
+    - TOMY JOSE MONTUFAR ZUNIGA
+    - ANROLD STANLY FORD MADRID
+'''
+import bcrypt
+pwd = bytes('pololo', 'utf-8')
+sal = bcrypt.gensalt()
+encriptado = bcrypt.hashpw(pwd, sal)
+
+def limpiar():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def Ejercicio4():
+    # Ejercicio 4: Escribir un programa que pida al usuario un numero entero y muestre por pantalla si
+    # es par o impar.
+    numero = 0
+    while True:
+        import os
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Ejercicio 4: Escribir un programa que pida al usuario un numero entero y")
+        print("muestre por pantalla si es par o impar.")
+        numero = int(input("Ingrese un numero entero (-1: salir): "))
+
+        if numero == -1:
+            break
+        
+        if numero % 2 == 0:
+            print("El numero ",numero," es par.")
+        else:
+            print("El numero ",numero," es impar.")
+        
+        input("Presione Enter para continuar...")
+
+def Ejercicio14():
+    # Ejercicio 14: Escribir un programa que pida al usuario un numero entero positivo y muestre por
+    # pantalla la cuenta atras desde ese numero hasta cero.
+    while True:
+        import os
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Ejercicio 14: Escribir un programa que pida al usuario un numero entero positivo")
+        print("y muestre por pantalla la cuenta atras desde ese numero hasta cero.")
+        entero = int(input("Ingrese un numero entero positivo (-1: salir): "))
+
+        if entero == -1:
+            break
+
+        if entero <= 0:
+            print("El numero ingresado debe ser mayor que 0, intentelo de nuevo...")
+            input("Presione Enter para continuar...")
+        else:
+            while entero != -1:
+                print(entero)
+                entero -= 1
+            input("Presione Enter para continuar...")
+    
+limpiar()
+
+usuario_correcto = "tomy"
+intentos = 3
+# Acceder al programa
+while intentos > 0:
+    limpiar()
+    usuario = input("Ingrese su usuario: ")
+    contraseña = bytes(input("Ingrese su contraseña: "), 'utf-8')
+
+    if usuario == usuario_correcto and bcrypt.checkpw(contraseña, encriptado):
+        print("Bienvenido al programa")
+        print(encriptado)
+        break
+    else:
+        intentos -= 1
+        print("Usuario o contraseña incorrecta, intente de nuevo (",intentos," restantes)")
+        input("Presione Enter para intentarlo de nuevo...") 
+else:
+    limpiar()
+    print("Ha excedido el numero de intentos permitidos.")
+    exit()
+
+# Menu principal
+while True:
+    limpiar()
+    print("Bienvenido al programa")
+    print("1. Inspeccionar departamentos y muncipios.")
+    print("2. Ingresar su cedula.")
+    print("3. Ejercicios del Examen IP")
+    print("4. Salir.")
+    selec = input("Elija una opcion: ")
+
+    # Inspeccionar departamentos y municipios
+    if selec == '1':
+        limpiar()
+        id = input("Ingrese el numero de departamento para ver sus municipios y su codigo (ejemplo 16): ")
+        if not id.isdigit() or len(id) != 2:
+            print("Codigo no valido, por favor intente de nuevo. (2 digitos y solo enteros)")
+            input("Presione Enter para continuar...")
+        else:
+            if id not in departamentos_municipios:
+                print("Departamento no encontrado.")
+                input("Presione Enter para continuar...")
+            else:
+                print("Departamento encontrado: ", departamentos_municipios[id]['nombre'])
+                print("-----------------------------------------------------")
+                print("Codigo\t\t Municipio")
+                print("-----------------------------------------------------")
+                for municipio in departamentos_municipios[id]['municipios']:
+                    print(municipio,"\t\t",departamentos_municipios[id]['municipios'][municipio])
+                    
+                input("Presione Enter para continuar...")
+    # Ingresar su cedula
+    elif selec == '2':
+        limpiar()
+        dni = input("Ingrese su numero de identidad: ")
+
+        if not dni.isdigit() or len(dni) != 13:
+            print("Numero de identidad invalido, intente de nuevo (13 digitos y solo enteros)")
+            input("Presione Enter para continuar...")
+        else:
+            id_departamento = dni[0:2]
+            id_municipio = dni[2:4]
+            id_anionacimiento = dni[4:8]
+            edad = 2024 - int(id_anionacimiento)
+
+            if id_departamento not in departamentos_municipios or id_municipio not in departamentos_municipios[id_departamento]['municipios']:
+                print("Datos no encontrados.")
+                input("Presione Enter para continuar...")
+            else:
+                departamento = departamentos_municipios[id_departamento]
+                municipio = departamento['municipios'].get(id_municipio)
+                print("Departamento: ", departamento['nombre'])
+                print("Municipio: ", municipio)
+                print("Edad: ", edad, " años")
+
+                if(edad>=21):
+                    print("Eres mayor de edad")
+                elif(edad>=18):
+                    print("Eres ciudadano")
+                else:
+                    print("Eres menor de edad")
+
+                input("Presione Enter para continuar...")
+    # Ejercicios desarrollados durante el examen del I Parcial
+    elif selec == '3':
+        limpiar()
+        print("Que programa desea ver?")
+        print("1. Ejercicio 4")
+        print("2. Ejercicio 14")
+        opcion = input("Elija una opcion: ")
+
+        if opcion == '1':
+            Ejercicio4()
+        elif opcion == '2':
+            Ejercicio14()
+        else:
+            print("Opcion no valida, intente de nuevo.")
+            input("Presione Enter para continuar...")
+
+    # Salir
+    elif selec == '4':
+        limpiar()
+        print("Programa finalizado.")
+        exit()
+    else:
+        print("Opcion no valida, intente de nuevo.")
+        input("Presione Enter para continuar...")
